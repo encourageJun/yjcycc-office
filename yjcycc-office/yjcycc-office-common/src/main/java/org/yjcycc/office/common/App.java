bde70d2836c4d414ea3d29d055ec9c4e62c11dff
yangjun <yangjun@xingxingcar.com>
2018-03-29 13:17:20 +0800

    新增: office-common

---
+++ yjcycc-office/yjcycc-office-common/src/main/java/org/yjcycc/office/common/App.java
@@ -0,0 +1,13 @@
+package org.yjcycc.office.common;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
